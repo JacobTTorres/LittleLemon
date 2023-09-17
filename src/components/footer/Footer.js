59a7dcde319f4b77b7faf33bf2217c9c96ede488
JacobTTorres <jacobttorres@hotmail.com>
2023-09-17 12:59:28 -0500

    start up files

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      Footer
+      <img src="" alt="" />
+      <nav className="doormatNav">
+        <ul className="footerNav">
+          <li className="footerNavLink">
+            <a href=""></a>
+          </li>
+          <li className="footerNavLink">
+            <a href=""></a>
+          </li>
+          <li className="footerNavLink">
+            <a href=""></a>
+          </li>
+          <li className="footerNavLink">
+            <a href=""></a>
+          </li>
+          <li className="footerNavLink">
+            <a href=""></a>
+          </li>
+          <li className="footerNavLink">
+            <a href=""></a>
+          </li>
+        </ul>
+      </nav>
+      <div className="contact">
+        <p className="address"></p>
+        <p className="phoneNumber"></p>
+        <p className="email"></p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Nav() {
+  return (
+    <nav>
+      <img src="" alt="Little Lemon img" />
+      <ul className="navLinks">
+        <li className="links"></li>
+        <li className="links"></li>
+        <li className="links"></li>
+        <li className="links"></li>
+        <li className="links"></li>
+        <li className="links"></li>
+      </ul>
+    </nav>
+  );
+}
