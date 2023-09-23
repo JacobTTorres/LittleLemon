e2682f9c5302c1b04cb4bf55e562857839c57227
JacobTTorres <77947250+JacobTTorres@users.noreply.github.com>
2023-09-23 13:03:07 -0500

    2 files modified, 22 deleted

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,37 +3,37 @@ import React from 'react';
 import FooterImg from '../../icons_assets/restaurant.jpg';
 
 export default function Footer() {
-	return (
-		<footer>
-			Footer
-			<img src={FooterImg} alt="" />
-			<nav className="doormatNav">
-				<ul className="footerNav">
-					<li className="footerNavLink">
-						<a href=""></a>
-					</li>
-					<li className="footerNavLink">
-						<a href=""></a>
-					</li>
-					<li className="footerNavLink">
-						<a href=""></a>
-					</li>
-					<li className="footerNavLink">
-						<a href=""></a>
-					</li>
-					<li className="footerNavLink">
-						<a href=""></a>
-					</li>
-					<li className="footerNavLink">
-						<a href=""></a>
-					</li>
-				</ul>
-			</nav>
-			<div className="contact">
-				<p className="address"></p>
-				<p className="phoneNumber"></p>
-				<p className="email"></p>
-			</div>
-		</footer>
-	);
+  return (
+    <footer>
+      Footer
+      {/* <img src={FooterImg} alt="" /> */}
+      <nav className="doormatNav">
+        <ul className="footerNav">
+          <li className="footerNavLink">
+            <a href=""></a>
+          </li>
+          <li className="footerNavLink">
+            <a href=""></a>
+          </li>
+          <li className="footerNavLink">
+            <a href=""></a>
+          </li>
+          <li className="footerNavLink">
+            <a href=""></a>
+          </li>
+          <li className="footerNavLink">
+            <a href=""></a>
+          </li>
+          <li className="footerNavLink">
+            <a href=""></a>
+          </li>
+        </ul>
+      </nav>
+      <div className="contact">
+        <p className="address"></p>
+        <p className="phoneNumber"></p>
+        <p className="email"></p>
+      </div>
+    </footer>
+  );
 }
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,17 +3,17 @@ import React from 'react';
 import Logo from '../../icons_assets/Logo.svg';
 
 export default function Nav() {
-	return (
-		<nav>
-			<img src={Logo} alt="Little Lemon img" />
-			<ul className="navLinks">
-				<li className="links"></li>
-				<li className="links"></li>
-				<li className="links"></li>
-				<li className="links"></li>
-				<li className="links"></li>
-				<li className="links"></li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav>
+      {/* <img src={Logo} alt="Little Lemon img" /> */}
+      <ul className="navLinks">
+        <li className="links"></li>
+        <li className="links"></li>
+        <li className="links"></li>
+        <li className="links"></li>
+        <li className="links"></li>
+        <li className="links"></li>
+      </ul>
+    </nav>
+  );
 }
