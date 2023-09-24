88ec08cdf7c3a3347ff31e978678afdb12af5ff5
JacobTTorres <77947250+JacobTTorres@users.noreply.github.com>
2023-09-24 12:44:59 -0500

    Navbar setup

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -6,15 +6,15 @@ import Logo from '../../icons_assets/Logo.svg';
 
 export default function Nav() {
   return (
-    <nav>
+    <nav className="topNavbar">
       {/* <img src={Logo} alt="Little Lemon img" /> */}
       <ul className="navLinks">
-        <li className="links"></li>
-        <li className="links"></li>
-        <li className="links"></li>
-        <li className="links"></li>
-        <li className="links"></li>
-        <li className="links"></li>
+        <li className="links">Home</li>
+        <li className="links">About</li>
+        <li className="links">Menu</li>
+        <li className="links">Reservations</li>
+        <li className="links">Order Online</li>
+        <li className="links">Login</li>
       </ul>
     </nav>
   );
