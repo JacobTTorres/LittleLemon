ece9679ee57b6167c0d060e08dbacf1c5a2ca5df
JacobTTorres <77947250+JacobTTorres@users.noreply.github.com>
2023-10-01 15:21:26 -0500

    2 files modified

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,38 +1,53 @@
 import React from 'react';
 
-import FooterImg from '../../icons_assets/restaurant.jpg';
+import './footer.css';
 
 export default function Footer() {
   return (
     <footer>
-      Footer
-      {/* <img src={FooterImg} alt="" /> */}
-      <nav className="doormatNav">
-        <ul className="footerNav">
-          <li className="footerNavLink">
-            <a href=""></a>
-          </li>
-          <li className="footerNavLink">
-            <a href=""></a>
-          </li>
-          <li className="footerNavLink">
-            <a href=""></a>
-          </li>
-          <li className="footerNavLink">
-            <a href=""></a>
-          </li>
-          <li className="footerNavLink">
-            <a href=""></a>
-          </li>
-          <li className="footerNavLink">
-            <a href=""></a>
-          </li>
-        </ul>
-      </nav>
-      <div className="contact">
-        <p className="address"></p>
-        <p className="phoneNumber"></p>
-        <p className="email"></p>
+      <img
+        src="https://loopstudio-jt-frontendmentor.netlify.app/static/media/image-interactive.1d330522c9ca6cf07a9c.jpg"
+        alt=""
+        className="footerImg"
+      />
+      <div className="doormatContainer">
+        <nav className="doormatNav">
+          <h4 className="doormatHeader">
+            Doormat <br /> Navigation
+          </h4>
+          <ul className="footerNav">
+            <li className="footerNavLink">
+              <a href="">Home</a>
+            </li>
+            <li className="footerNavLink">
+              <a href="">About</a>
+            </li>
+            <li className="footerNavLink">
+              <a href="">Menu</a>
+            </li>
+            <li className="footerNavLink">
+              <a href="">Reservations</a>
+            </li>
+            <li className="footerNavLink">
+              <a href="">Order Online</a>
+            </li>
+            <li className="footerNavLink">
+              <a href="">Login</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="contact">
+          <h4 className="contactHead">Contact</h4>
+          <p className="address">Address</p>
+          <p className="phoneNumber">Phone Number</p>
+          <p className="email">Email</p>
+        </div>
+        <div className="socialMedia">
+          <h4 className="MediaHead">Social Media Links</h4>
+          <p className="address">Address</p>
+          <p className="phoneNumber">Phone Number</p>
+          <p className="email">Email</p>
+        </div>
       </div>
     </footer>
   );
