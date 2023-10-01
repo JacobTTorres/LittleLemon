d3c22e3b7db1d146e6bbe2b8cec4d688ca4ab704
JacobTTorres <77947250+JacobTTorres@users.noreply.github.com>
2023-10-01 14:14:12 -0500

    bottons adjusted just needs actual pictures

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -12,56 +12,74 @@ export default function Main() {
         </div>
         <div className="specBottom">
           <div className="card">
-            <img src="" alt="" />
-            <div className="cardTop">
-              <p className="price">$12.99</p>
-            </div>
-            <h4 className="itemTitle">Greek salad</h4>
-            <div className="cardMid">
-              <p className="cardInfo">
-                The famous greek salad of crispy lettuce, peppers, olives and
-                our Chicago style feta cheese, garnished with crunchy garlic and
-                rosemary croutons.
-              </p>
-            </div>
-            <div className="cardBottom">
-              <h5 className="orderDeli">Order a delivery</h5>
-              <span class="material-symbols-outlined">takeout_dining</span>
-            </div>
-          </div>
-          <div className="card">
-            <img src="" alt="" />
-            <div className="cardTop">
-              <p className="price">$ 5.99</p>
-            </div>
-            <h4 className="itemTitle">Bruchetta</h4>
-            <div className="cardMid">
-              <p className="cardInfo">
-                Our Bruschetta is made from grilled bread that has been smeared
-                with garlic and seasoned with salt and olive oil.
-              </p>
-            </div>
-            <div className="cardBottom">
-              <h5 className="orderDeli">Order a delivery</h5>
-              <span class="material-symbols-outlined">takeout_dining</span>
+            <img
+              src="https://loopstudio-jt-frontendmentor.netlify.app/static/media/image-interactive.1d330522c9ca6cf07a9c.jpg"
+              alt=""
+              className="cardImg"
+            />
+            <div className="lowerCard">
+              <div className="specCardTop">
+                <h4 className="itemTitle">Greek salad</h4>
+                <p className="price">$12.99</p>
+              </div>
+              <div className="cardMid">
+                <p className="cardInfo">
+                  The famous greek salad of crispy lettuce, peppers, olives and
+                  our Chicago style feta cheese, garnished with crunchy garlic
+                  and rosemary croutons.
+                </p>
+              </div>
+              <div className="specCardBottom">
+                <h5 className="orderDeli">Order a delivery</h5>
+                <span class="material-symbols-outlined">takeout_dining</span>
+              </div>
             </div>
           </div>
           <div className="card">
-            <img src="" alt="" />
-            <div className="cardTop">
-              <p className="price">$ 5.00</p>
+            <img
+              src="https://loopstudio-jt-frontendmentor.netlify.app/static/media/image-interactive.1d330522c9ca6cf07a9c.jpg"
+              alt=""
+              className="cardImg"
+            />
+            <div className="lowerCard">
+              <div className="specCardTop">
+                <h4 className="itemTitle">Bruchetta</h4>
+                <p className="price">$ 5.99</p>
+              </div>
+              <div className="cardMid">
+                <p className="cardInfo">
+                  Our Bruschetta is made from grilled bread that has been
+                  smeared with garlic and seasoned with salt and olive oil.
+                </p>
+              </div>
+              <div className="specCardBottom">
+                <h5 className="orderDeli">Order a delivery</h5>
+                <span class="material-symbols-outlined">takeout_dining</span>
+              </div>
             </div>
-            <h4 className="itemTitle">Lemon Dessert</h4>
-            <div className="cardMid">
-              <p className="cardInfo">
-                This comes straight from grandma’s recipe book, every last
-                ingredient has been sourced and is as authentic as can be
-                imagined.
-              </p>
-            </div>
-            <div className="cardBottom">
-              <h5 className="orderDeli">Order a delivery</h5>
-              <span class="material-symbols-outlined">takeout_dining</span>
+          </div>
+          <div className="card">
+            <img
+              src="https://loopstudio-jt-frontendmentor.netlify.app/static/media/image-interactive.1d330522c9ca6cf07a9c.jpg"
+              alt=""
+              className="cardImg"
+            />
+            <div className="lowerCard">
+              <div className="specCardTop">
+                <h4 className="itemTitle">Lemon Dessert</h4>
+                <p className="price">$ 5.00</p>
+              </div>
+              <div className="cardMid">
+                <p className="cardInfo">
+                  This comes straight from grandma’s recipe book, every last
+                  ingredient has been sourced and is as authentic as can be
+                  imagined.
+                </p>
+              </div>
+              <div className="specCardBottom">
+                <h5 className="orderDeli">Order a delivery</h5>
+                <span class="material-symbols-outlined">takeout_dining</span>
+              </div>
             </div>
           </div>
         </div>
